e6b0ef68623de624594f7c271208eb837089d0f0
Kenny <101839505+khui0@users.noreply.github.com>
2025-02-27 13:44:21 -0500

    feat: vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import "../app.css";
   let { children } = $props();
+
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+  inject({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
